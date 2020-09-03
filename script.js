9422c13701a5fca482b6a44a63399c9fb84397e1
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-09-03 22:35:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// question 1
+const cat = {
+  complain: function() {
+    console.log("Meow!");
+  }
+};
+    
+// question 2
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// question 2
 
