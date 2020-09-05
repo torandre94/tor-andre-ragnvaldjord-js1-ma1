393e95b00aab6e8813a068504c56b75cb2d81dcf
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-09-05 13:22:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ heading.innerHTML = "Updated heading";
 
 // question 3
 heading.style.fontSize = "2em";
+
+// question 4
+<h3 class="subheading">Heading</h3>
+
+// question 5
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+  console.dir(paragraphs[i]);
+  paragraphs[i].style.color = "red";
+}
+
+// question 6
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+// question 7
+
+
+
