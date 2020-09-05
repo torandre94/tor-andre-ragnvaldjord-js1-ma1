ec4d558a343243c2850ded9b425850e28145fab4
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-09-05 16:31:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const cat = {
     console.log("Meow!");
   }
 };
+
+cat.complain();
     
 // question 2
 const heading = document.querySelector("h3");
@@ -31,6 +33,34 @@ resultsContainer.innerHTML = "<p>New paragraph</p>";
 resultsContainer.style.backgroundColor = "yellow";
 
 // question 7
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function nameList(list) {
+  for(let i = 0; i< list.length; i++) {
+    console.log(list[i].name);
+  }
+}
+
+nameList(cats);
+
+// question 7
+
+
+
+
+
 
 
 
